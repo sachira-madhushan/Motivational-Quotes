af360b0d8e9547676b80e17370d8d66af76977be
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2023-09-03 13:14:10 +0530

    Completed

--- Assets/ToastMessage.cs
+++ Assets/ToastMessage.cs
@@ -3,7 +3,6 @@ using UnityEngine.UI;
 
 public class ToastMessage : MonoBehaviour
 {
-    public Text messageText;
     public float displayDuration = 2.0f; // Adjust the display duration as needed
     private float displayTimer = 0.0f;
     private bool displaying = false;
